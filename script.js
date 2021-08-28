87b4c4b53a0f1a17999e7ce1b9a774f3d8138f89
crrdlx <40270030+crrdlx@users.noreply.github.com>
2021-08-28 09:18:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ form.addEventListener("submit", e => {
   }
 
   //ajax here
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=imperial`;
 
   fetch(url)
     .then(response => response.json())
